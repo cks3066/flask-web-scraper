d9c4785e0a14cb6b1731dab432cb11c4d5295f9e
Seungchan On <62434898+cks3066@users.noreply.github.com>
2022-01-25 14:16:41 +0000

    feat: connect render_template

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScrapper")
 
 @app.route("/")
 def home():
-  return "Hello! Welcome to mi casa"
+  return render_template("potato.html")
 
-@app.route("/contact")
-def contact():
-  return "Contact me"
+@app.route("/<username>")
+def username(username):
+  return f"Hello Your Name is {username}, How are you doing"
 
 app.run(host="0.0.0.0")
 
